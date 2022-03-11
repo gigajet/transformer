7a0de8aca8b1d0c7e333376654e2ecdd4c17203e
gigajet <gigajet08092000@gmail.com>
2022-03-11 14:56:14 +0700

    upd: MultiHeadAttention

--- layer/MultiheadAttention.py
+++ layer/MultiheadAttention.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-from layer.ScaledDotProductAttention import ScaledDotProductAttention
+from ScaledDotProductAttention import ScaledDotProductAttention
 
 class MultiheadAttention (nn.Module):
     """
@@ -17,16 +17,13 @@ class MultiheadAttention (nn.Module):
             d_v_out: int) -> None:
         super().__init__()
         assert(d_k % n_head == 0 and d_v % n_head == 0)
+        self.n_head = n_head
         self.Wq = nn.Linear(d_k_in, d_k)
         self.Wk = nn.Linear(d_k_in, d_k)
         self.Wv = nn.Linear(d_v_in, d_v)
         self.att = ScaledDotProductAttention()
         self.Wo = nn.Linear(d_v, d_v_out)
 
-    # TODO phần Linear thì như nhân ma trận lớn được
-    # Nhưng phần Attention thì không thể ăn gian (cái d_k)
-    # Phải split rồi về sau concat đàng hoàng.
-    # Còn phần output thì chỉ nhân một ma trận thôi
     """
     Q, K: (*, n, d_k_in)
     V: (*, n, d_v_in)
@@ -34,8 +31,53 @@ class MultiheadAttention (nn.Module):
     output: (*, n, d_v_out)
     """
     def forward(self, Q, K, V, mask=None):
-        Q.matmul_(self.Wq)  # (*, n, d_k)
-        K.matmul_(self.Wk)
+        Q = self.Wq(Q)  # (*, n, d_k)
+        K = self.Wk(K)  # (*, n, d_k)
+        V = self.Wv(V)  # (*, n, d_v)
+        Q,K,V = self.split(Q), self.split(K), self.split(V)
+        an = self.att(Q,K,V,mask) # (*, n_head, n, d_v//n_head)
+        an = self.concat(an)
+        an = self.Wo(an)
+        return an
+
+    """
+    A: (*, n, d)
+    output: (*, n_head, n, d/n_head)
+    """
+    def split(self, A):
+        n, d = A.shape[-2::]
+        A = A.transpose(-2,-1).reshape(*A.shape[:-2], self.n_head, d//self.n_head, n).transpose(-2,-1)
+        return A
+
+    """
+    A: (*, n_head, n, d//n_head)
+    output: (*, n, d)
+    """
+    def concat(self, A):
+        n, ddiv = A.shape[-2::]
+        d = ddiv * self.n_head
+        A = A.transpose(-2,-1).reshape(*A.shape[:-3], d, n).transpose(-2,-1)
+        return A
 
 if __name__=="__main__":
+    z = torch.tensor([
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16],
+    ]) # (4, 4)
+    n, d = z.shape[-2::]
+    z = z.transpose(-2,-1).reshape(*z.shape[:-2], 2, d//2, n).transpose(-2,-1)
+    print(z.shape,z) # (2,4,2)
+
+    z = z.transpose(-2,-1).reshape(*z.shape[:-3], 4, 4).transpose(-2,-1)
+    print(z.shape,z) # (4,4)
+
+    z = torch.tensor([
+        [[1,1,1,1],[2,2,2,2],[3,3,3,3]],
+        [[4,4,4,4],[5,5,5,5],[6,6,6,6]],
+    ]).float()
+    y = torch.ones((2,4,3))
+    z = z @ y
+    print(z.shape,z)
     pass
