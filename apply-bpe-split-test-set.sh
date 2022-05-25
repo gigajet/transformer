23b9e697c460c035cfe666d677be6ceed7f66075
gigajet <gigajet08092000@gmail.com>
2022-05-26 00:09:43 +0700

    upd: unknown

--- apply-bpe-split-test-set.sh
+++ apply-bpe-split-test-set.sh
@@ -1,8 +1,9 @@
 
 src=en
 tgt=vi
-BPE_CODE=$prep/code
+BPEROOT=subword-nmt/subword_nmt
 prep=iwslt15.tokenized.en-vi
+BPE_CODE=$prep/code
 tmp=$prep/tmp
 for L in $src $tgt; do
     for f in test.00-10.$L test.11-20.$L test.21-30.$L test.31-40.$L test.41-.$L; do
