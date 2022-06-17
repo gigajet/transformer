f72d55644453d035512cb73e0ac21dab9f9ddf65
gigajet <gigajet08092000@gmail.com>
2022-06-18 02:12:20 +0700

    upd: add baseline to paired-bootstrap

--- paired-bootstrap/script.sh
+++ paired-bootstrap/script.sh
@@ -3,7 +3,7 @@ num_bootstrap_sample=1000
 src_path=cmp-p9_32-p10_16/source.txt
 gt_path=cmp-p9_32-p10_16/ground_truth.txt
 model_A_path=cmp-p9_32-p10_16/proposal9_32.txt
-model_B_path=cmp-p9_32-p10_16/proposal10_16.txt
+model_B_path=cmp-p9_32-p10_16/baseline.txt
 output_dir=output
 
 python significance.py -n $num_broad_sample -m $num_bootstrap_sample \
