720f5950360c9323736df3266461a44da8dbe59f
gigajet <gigajet08092000@gmail.com>
2022-06-21 13:52:07 +0700

    eval script: eval all subtest

--- scripts/evaluation_europarl-v7.py
+++ scripts/evaluation_europarl-v7.py
@@ -16,27 +16,32 @@ if __name__=="__main__":
     user_dir = sys.argv[3]
     output_dir = sys.argv[4] if len(sys.argv)>4 else 'eval'
     os.makedirs(output_dir, exist_ok=True)
+
+    real_setnames = ['test','test.00-10','test.11-20','test.21-30','test.31-40','test.41-']
+    preprocessed_setnames = ['test','test1','test2','test3','test4','test5']
     for model_name in os.listdir(checkpoint_dir):
         language_pair = model_name.split('.')[0][-5::]
-        cmd = """\
+        for setname, fairseq_setname in zip(real_setnames, preprocessed_setnames):
+            cmd = """\
 fairseq-generate {full_dataset_dir} \\
     --path {full_checkpoint_dir} \\
-    --log-file {full_logfile_dir} \\
+    --gen-subset {fairseq_setname}
     --batch-size 128 --beam 5 --remove-bpe \\
     --user-dir {user_dir}""".format(
-            full_dataset_dir=os.path.join(dataset_dir, language_pair),
-            full_checkpoint_dir=os.path.join(checkpoint_dir,model_name,"checkpoint_best.pt"),
-            full_logfile_dir=os.path.join(output_dir,model_name,model_name+".txt"),
-            user_dir=user_dir,
-            )
-        os.makedirs(os.path.join(output_dir,model_name), exist_ok=True)
-        with open(os.path.join(output_dir,model_name,"eval_command.log"), 'w') as f_cmd, \
-            open(os.path.join(output_dir,model_name,"stdout-"+model_name+".log"), 'w') as f_out, \
-            open(os.path.join(output_dir,model_name,"stderr-"+model_name+".log"), 'w') as f_err:
-            f_cmd.write(cmd)
-            f_cmd.close()
-            exit_code = subprocess.call(cmd, shell=True, stdout=f_out, stderr=f_err)
-            print("["+model_name+']','fairseq_generate returns',exit_code)
-            if exit_code != 0:
-                print('Unsuccessful')
-                break
+                full_dataset_dir=os.path.join(dataset_dir, language_pair),
+                full_checkpoint_dir=os.path.join(checkpoint_dir,model_name,"checkpoint_best.pt"),
+                full_logfile_dir=os.path.join(output_dir,model_name,model_name+".txt"),
+                fairseq_setname=fairseq_setname
+                user_dir=user_dir,
+                )
+            os.makedirs(os.path.join(output_dir,model_name), exist_ok=True)
+            with open(os.path.join(output_dir,model_name,"eval_command.log"), 'w') as f_cmd, \
+                open(os.path.join(output_dir,model_name,"stdout."+setname+".log"), 'w') as f_out, \
+                open(os.path.join(output_dir,model_name,"stderr."+setname+".log"), 'w') as f_err:
+                f_cmd.write(cmd)
+                f_cmd.close()
+                exit_code = subprocess.call(cmd, shell=True, stdout=f_out, stderr=f_err)
+                print("["+model_name+']','fairseq_generate returns',exit_code)
+                if exit_code != 0:
+                    print('Unsuccessful')
+                    break
